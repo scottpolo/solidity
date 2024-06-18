874b1d4b55eb55c00229126ade9706f55f0f3c81
Matheus Aguiar <matheus.pit@gmail.com>
2024-06-17 22:22:34 -0300

    Replace errors/warning/info empty check with proper errors only check

--- test/tools/yulopti.cpp
+++ test/tools/yulopti.cpp
@@ -93,7 +93,7 @@ public:
 		try
 		{
 			m_ast = yul::Parser(errorReporter, m_dialect).parse(_charStream);
-			if (!m_ast || !errorReporter.errors().empty())
+			if (!m_ast || errorReporter.hasErrors())
 			{
 				std::cerr << "Error parsing source." << std::endl;
 				printErrors(_charStream, errors);
@@ -105,7 +105,7 @@ public:
 				errorReporter,
 				m_dialect
 			);
-			if (!analyzer.analyze(*m_ast) || !errorReporter.errors().empty())
+			if (!analyzer.analyze(*m_ast) || errorReporter.hasErrors())
 			{
 				std::cerr << "Error analyzing source." << std::endl;
 				printErrors(_charStream, errors);
